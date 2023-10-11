d12fa919f7b07f789edc63518260fd83fe5bd335
dd5287082 <145669756+dd5287082@users.noreply.github.com>
2023-10-11 13:17:51 +0530

    Add files via upload

---
+++ Challenge 2.1.py
@@ -0,0 +1,40 @@
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
+Output: 
+ 
+
+Hello !!! Welcome to Deposit&Withdrawal Machine
+Enter amount to be deposited: 
+ Amount Deposited: 1000.0
+Enter amount to be withdrawn: 
+ You Withdrew: 500.0
+
+ Net Available Balance = 500.0
